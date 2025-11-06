877ca73b5637fd99ddd7663d5243213943c10327
ToayoOnajobi <122939284+ToayoOnajobi@users.noreply.github.com>
2025-11-06 01:43:19 +0000

    Deploying to gh-pages from @ ToayoOnajobi/ToayoOnajobi.github.io@0be1995702ac07596f4ae6eb5ff393ac2bf36c3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,9 +461,9 @@ ninja.data = [{
           description: "Personal Project of a Video-Game Classic",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-weatherapp",
+          title: 'WeatherApp',
+          description: "A WeatherApp developed by Java, Spring Boot and React",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
